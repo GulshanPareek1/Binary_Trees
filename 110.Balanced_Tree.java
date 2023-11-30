0e820c00eefe54f32253ef6293a9065b1a9af674
GulshanPareek1 <100085680+GulshanPareek1@users.noreply.github.com>
2023-11-30 19:16:20 +0530

    Update 110.Balanced_Tree.java

--- 110.Balanced_Tree.java
+++ 110.Balanced_Tree.java
@@ -22,3 +22,56 @@ class Solution {
 		return Math.max(leftHeight, rightHight) + 1;
     }
 }
+
+
+
+// same solution but done by me again after thinking 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if(root==null)
+        {
+            return true;
+        }
+        return height(root)!=-1;
+    }
+    
+    private int height(TreeNode root)
+    {
+        if(root==null)
+        {
+            return 0;
+        }
+        int left = height(root.left);
+        if(left==-1)
+        {
+            return -1;
+        }
+        int right = height(root.right);
+        if(right==-1)
+        {
+            return -1;
+        }
+        // add condition of unbalanced tree 
+        if(Math.abs(left-right)>1)
+        {
+            return -1;
+        }
+        
+        return 1+Math.max(left,right);
+    }
+}
