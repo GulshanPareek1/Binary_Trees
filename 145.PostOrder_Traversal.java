ca72e4f8d8dff4e1fe0461c8aaf67393d9f26c2b
GulshanPareek1 <100085680+GulshanPareek1@users.noreply.github.com>
2023-12-01 23:20:05 +0530

    Update 145.PostOrder_Traversal.java

--- 145.PostOrder_Traversal.java
+++ 145.PostOrder_Traversal.java
@@ -23,3 +23,35 @@ class Solution {
         
     }
 }
+
+
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        //postorder traversal iterative
+        LinkedList<Integer> ans = new LinkedList<Integer>();
+        //List<Integer> ans = new ArrayList<Integer>();
+        Stack<TreeNode> st = new Stack();
+        if(root==null)
+        {
+            return ans;
+        }
+              
+        st.push(root);
+        while(!st.isEmpty())
+        {
+            TreeNode current = st.pop();
+            ans.addFirst(current.val);
+            //ans.add(0,current.val);
+            
+            if(current.left!=null)
+            {
+                st.push(current.left);
+            }
+            if(current.right!=null)
+            {
+                st.push(current.right);
+            }
+        }
+        return ans;
+    }
+}
